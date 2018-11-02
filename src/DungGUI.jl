91e7b38633ad5a53ef1029397af29e2a80ba751f
yakir12 <12.yakir@gmail.com>
2018-11-02 23:58:49 +0100

    tons of fixes

--- src/DungGUI.jl
+++ src/DungGUI.jl
@@ -11,7 +11,7 @@ const source = joinpath(homedir(), "coffee source")
 exiftool_base = abspath(joinpath(pathof(DungGUI), "..", "..", "deps", "src", "exiftool", "exiftool"))
 const exiftool = exiftool_base*(Sys.iswindows() ? ".exe" : "")
 
-badfile(source, x) = x[1] == '.' || last(splitext(x)) ∉ [".MTS", ".mp4", ".MP4", ".avi", ".AVI", ".mpg", ".MPG", ".mov", ".MOV"] || !isfile(joinpath(source, x))
+badfile(x) = x[1] == '.' || last(splitext(x)) ∉ [".MTS", ".mp4", ".MP4", ".avi", ".AVI", ".mpg", ".MPG", ".mov", ".MOV"] || !isfile(joinpath(source, x))
 
 function baddatetime(x)
     try
@@ -22,36 +22,50 @@ function baddatetime(x)
     end
 end
 
-function getsomething(msg, wrn, fun; default = "")
-    println(msg)
-    println(default)
-    l = strip(readline(stdin))
-    if isempty(l)
-        l = default
-    end
-    while fun(l)
-        @warn wrn
-        println(msg)
-        l = strip(readline(stdin))
-    end
-    l
+const df = DateFormat("YYYY:mm:dd HH:MM:SSzzzz")
+
+function getalldatetimes(file)
+    str = read(`$exiftool -j $file`, String)
+    jsn = first(JSON.parse(str))
+    [DateTime(ZonedDateTime(v, df)) for (k,v) in jsn if occursin(r"date"i, k)]
 end
 
-df = DateFormat("YYYY:mm:dd HH:MM:SSzzzz")
-
-function getearliest(file)
-    str = read(`$exiftool -j $file`, String)
-    jsn = JSON.parse(str)
-    earliest = ZonedDateTime(DateTime(9999,12,30), tz"UTC")
-    for (k, v) in first(jsn)
-        if occursin(r"date"i, k)
-            d = ZonedDateTime(v, df)
-            if d < earliest
-                earliest = d
-            end
+function getdatetime(file)
+    alldts = getalldatetimes(file)
+    opts = string.(alldts)
+    push!(opts, "Enter other")
+    lastn = length(opts)
+    menu = RadioMenu(opts)
+    dti = request("Choose a creation date & time for \"$file\"?", menu)
+    if dti == lastn
+        l = strip(readline(stdin))
+        while baddatetime(l)
+            @warn "the format of the date or time is wrong, try something like `2018-11-01T12:47`"
+            l = strip(readline(stdin))
         end
+        DateTime(l)
+    else
+        alldts[dti]
     end
-    DateTime(earliest)
+end
+
+parse_floating_seconds(x) = Time(0) + Nanosecond(round(Int, x*10^9))
+
+function parsetime(x)
+    n = count(isequal(':'), x)
+    t = if n == 0
+        s = parse(Float64, x)
+        parse_floating_seconds(s)
+    elseif n == 1
+        m, s = parse.(Float64, split(x, ':'))
+        parse_floating_seconds(s) + Minute(m)
+    elseif n == 2
+        h, m, s = parse.(Float64, split(x, ':'))
+        parse_floating_seconds(s) + Minute(m) + Hour(h)
+    else
+        error("""one too many colons in this "time": $x""")
+    end
+    t - Time(0)
 end
 
 function getduration(file)
@@ -61,12 +75,10 @@ function getduration(file)
     parsetime(txt)
 end
 
-function registervideos(source, unregistered)
+function registervideos(unregistered)
     videos = Vector{Dict{String, Any}}()
-    wrn = "the format of the date or time is wrong, try `2018-11-01T12:47:51.783`, try again"
     fn = first(unregistered)
-    msg = "What is the creation date & time of \"$fn\"?"
-    dt = getsomething(msg, wrn, baddatetime, default = getearliest(joinpath(source, fn)))
+    dt = getdatetime(joinpath(source, fn))
     duration = getduration(joinpath(source, fn))
     push!(videos, Dict("file" => fn, "datetime" => dt, "duration_ns" => duration))
     for i in 2:length(unregistered)
@@ -76,27 +88,6 @@ function registervideos(source, unregistered)
         push!(videos, Dict("file" => fn, "datetime" => dt, "duration_ns" => duration))
     end
     videos
-end
-
-format_run(folder, id, date, comment) = "FOLDER: $folder, ID: $id, DATE: $date, COMMENT: $comment"
-
-parse_floating_seconds(x) = Time(0) + Nanosecond(round(Int, x*10^9))
-function parsetime(x)
-    t = if !occursin(":", x)
-        s = parse(Float64, x)
-        parse_floating_seconds(s)
-    else
-        y = split(x, ':')
-        hms = parse.(Float64, y)
-        if length(hms) == 2
-            m, s = hms
-            parse_floating_seconds(s) + Minute(h)
-        else
-            h, m, s = hms
-            parse_floating_seconds(s) + Minute(m) + Hour(h)
-        end
-    end
-    t - Time(0)
 end
 
 function getstarttime(duration)
@@ -109,12 +100,12 @@ function getstarttime(duration)
     end
 end
 
-function getstoptime(startfile, stopfile, starttime, duration)
+function getstoptime(samefile, starttime, duration)
     while true
         println("When in that file did the calibration end?")
         l = strip(readline(stdin))
         stoptime = parsetime(l)
-        bad = startfile == stopfile ? !(starttime ≤ stoptime ≤ duration) : stoptime > duration
+        bad = samefile ? !(starttime ≤ stoptime ≤ duration) : stoptime > duration
         bad ? println("time specified is longer than the duration of the video, $(nano2sec(duration)) sec (or shorter than the starting time if the start and end videos are the same). Try again") : return stoptime
     end
 end
@@ -124,8 +115,9 @@ nano2sec(x) = x/Nanosecond(Second(1))
 makeft(file, time) = Dict("file" => file, "nanosecond" => time)
 makepoi(name; startfile = "", starttime = Nanosecond(0), stopfile = "", stoptime = Nanosecond(0)) = Dict{String, Any}("name" => name, "start" => makeft(startfile, starttime), "stop" => makeft(stopfile, stoptime))
 
+const calibration_pois = ["Moving_checkerboard_calibration", "Stationary_checkerboard_calibration"]
+
 function getpoi(videos)
-    calibration_pois = ["Moving_checkerboard_calibration", "Stationary_checkerboard_calibration"]
     menu = RadioMenu(calibration_pois)
     cali = request("What kind of calibration is this?", menu)
 
@@ -139,7 +131,7 @@ function getpoi(videos)
     if cali == 1
         stopfilei = request("Select the video file where the calibration ended:", menu)
         stopfile = videosfiles[stopfilei]
-        stoptime = getstoptime(startfile, stopfile, starttime, videos[stopfilei]["duration_ns"])
+        stoptime = getstoptime(startfile == stopfile, starttime, videos[stopfilei]["duration_ns"])
     else
         stopfile = startfile
         stoptime = starttime
@@ -156,17 +148,24 @@ function notcalibrated(target, db)
     return true
 end
 
+function format_run(x) 
+    msg = ""
+    for k in ["run_folder", "id", "date", "comment"]
+        if haskey(x, k)
+            msg *= string(uppercase.(k), ": ", x[k])
+        end
+    end
+    msg
+end
+
 function getruns(db)
-    opts = get.(db["experiments"], "name", nothing)
+    remainingexps = filter(x -> any(notcalibrated(k, db) for k in keys(x["runs"])), db["experiments"])
+    opts = get.(remainingexps, "name", nothing)
     menu = RadioMenu(opts)
     expi = request("Which experiment is this calibration in?", menu)
 
-    runuuids = collect(filter(k -> notcalibrated(k, db), keys(db["experiments"][expi]["runs"])))
-    opts = String[]
-    for uuid in runuuids
-        x = db["experiments"][expi]["runs"][uuid]
-        push!(opts, format_run(x["run_folder"], x["id"], x["date"], x["comment"]))
-    end
+    runuuids = collect(filter(k -> notcalibrated(k, db), keys(remainingexps[expi]["runs"])))
+    opts = [format_run(remainingexps[expi]["runs"][k]) for k in runuuids]
     menu = MultiSelectMenu(opts)
     runi = request("Select all the runs that are calibrated by this calibration", menu)
     runuuids[collect(runi)]
@@ -191,30 +190,8 @@ function convert2int!(db)
     end
 end
 
-
-function main()
-
-    db = open(joinpath(source, "metadata.json"), "r") do io
-        JSON.parse(io)
-    end
-
-    convert2ns!(db)
-
-    registered = [v["file"] for x in db["videos"] for v in x]
-    both = filter(x -> !badfile(source, x), readdir(source))
-    unregistered = setdiff(both, registered)
-    if isempty(unregistered)
-        opts = String[string(get.(x, "file", nothing)) for x in db["videos"]]
-        menu = RadioMenu(opts)
-        vidi = request("No newly added videos were detected. Which of the already registered videos would you like to work with?", menu)
-        videos = db["videos"][vidi]
-    else
-        videos = registervideos(source, unregistered)
-        push!(db["videos"], videos)
-    end
-
-    more = true
-    while more
+function addpoiruns!(db, videos)
+    while true
         poi = getpoi(videos)
         runuuid = getruns(db)
 
@@ -226,11 +203,35 @@ function main()
 
         println("Are there any more calibrations in these video file/s? [y/[n]]")
         l = strip(readline(stdin))
-        more = l == "y"
+        l ≠ "y" && return nothing
+    end
+end
+
+
+function main()
+
+    db = open(joinpath(source, "metadata.json"), "r") do io
+        JSON.parse(io)
+    end
+    convert2ns!(db)
+
+
+    registered = [v["file"] for x in db["videos"] for v in x]
+    both = filter(!badfile, readdir(source))
+    unregistered = setdiff(both, registered)
+    if isempty(unregistered)
+        opts = String[string(get.(x, "file", nothing)) for x in db["videos"]]
+        menu = RadioMenu(opts)
+        vidi = request("No newly added videos were detected. Which of the already registered videos would you like to work with?", menu)
+        videos = db["videos"][vidi]
+    else
+        videos = registervideos(unregistered)
+        push!(db["videos"], videos)
     end
 
-    convert2int!(db)
+    addpoiruns!(db, videos)
 
+    convert2int!(db)
     open(joinpath(source, "metadata.json"), "w") do io
         JSON.print(io, db, 3)
     end
